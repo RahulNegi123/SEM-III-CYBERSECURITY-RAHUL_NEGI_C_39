5085be79596820052fdbce44c5305b76d77dffa8
Rahul Negi <pentiumrahul2001@gmail.com>
2020-08-27 20:07:39 +0530

    3rd commit

--- Assignment 3/2.py
+++ Assignment 3/2.py
@@ -1,4 +1,4 @@
-def calc(a,b):
+def lesser_of_two_evens(a,b):
  if a % 2 == 0 and b % 2 == 0:
   if a < b:
    return(a)
@@ -14,5 +14,5 @@ def calc(a,b):
 
 num1=int(input("Enter first number : \n"))
 num2=int(input("Enter second number : \n"))
-answer=calc(num1,num2)
+answer=lesser_of_two_evens(num1,num2)
 print(answer)
